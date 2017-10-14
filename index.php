24d8e3f8fa19c10140ed2356d0241dfb0421015c
grotog <30875342+grotog@users.noreply.github.com>
2017-10-14 19:12:36 -0300

    a

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+
+    <meta charset="utf-8">
+  <?php
+      require '_html/header.htm';
+
+     ?>
+
+     <link rel="stylesheet" href="_css/master.css">
+
+  </head>
+  <body id="bodymaster">
+
+
+aaaaa
+aaaa
+
+
+
+  </body>
+  <footer>
+
+
+<?php
+//require '/_html/footer.htm';
+ ?>
+  </footer>
+</html>
